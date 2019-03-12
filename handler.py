8c600f03b9053e47caf10cefe921e611b805c853
canfuu <canfuu@163.com>
2019-03-12 18:26:39 +0800

    add .gitignore
    修复了handler会对源文件造成修改的bug

--- handler.py
+++ handler.py
@@ -11,11 +11,12 @@ sys.setrecursionlimit(2000)
 file_paths = scanner.scan('/Users/canfuu/Documents/python/code/final_project/openapi-directory',
                           match_file_name='swagger.yaml')
 for path in file_paths:
+    text = ""
     with open(path, 'r', encoding='utf-8') as file:
         text = file.read()
-        try:
-            data = yaml.load(text)
-            parser.insert(data, 'localhost', 'root', 'root', 'api_swagger_source')
-        except Exception as e:
-            print("不符合yaml语法规则，文件内容格式错误" + path)
-            print(e)
+    try:
+        data = yaml.load(text)
+        parser.insert(data, 'localhost', 'root', 'root', 'api_swagger_source')
+    except Exception as e:
+        print("不符合yaml语法规则，文件内容格式错误" + path)
+        print(e)
