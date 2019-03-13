c12445a6606e3f5a6e4a8411a39410641f27f8cb
canfuu <canfuu@163.com>
2019-03-13 18:14:20 +0800

    bug fix

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ file = open(sys.argv[1], 'r', encoding='utf-8')
 context = file.read()
 
 x = yaml.load(context)
-
+x['file_path'] = sys.argv[1]
 parser.insert(x, sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], build=True)
