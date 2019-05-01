27c606e8104d7309fed040e8a74b7c0d0af39fad
canfuu <canfuu@163.com>
2019-05-01 17:43:44 +0800

    defination of method

---
+++ suppose/trend_painter.py
@@ -0,0 +1,32 @@
+# code=utf-8
+
+def uri_layer():
+    pass
+
+
+def api_resource():
+    pass
+
+
+def endpoint():
+    pass
+
+
+def method():
+    pass
+
+
+def response_layer():
+    pass
+
+
+def request_restriction():
+    pass
+
+
+def request_param():
+    pass
+
+
+def request_param_location():
+    pass
